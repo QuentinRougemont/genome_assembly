428fec6ebd7636cfa0d45d830381624de048c800
Quentin Rougemont <quentinrougemont@orange.fr>
2021-10-27 17:18:10 +0200

    Update 02.download_contaminant_human_and_insect.sh

--- 01.scripts/02.download_contaminant_human_and_insect.sh
+++ 01.scripts/02.download_contaminant_human_and_insect.sh
@@ -1,16 +1,21 @@
 
 #use ncbi dowload to dowload the data: https://github.com/kblin/ncbi-genome-download
-ncbi-genome-download --formats fasta --refseq-categories reference bacteria,viral,fungi,protozoa,arachaea  
+#ncbi-genome-download --formats fasta --refseq-categories reference bacteria,viral,fungi,protozoa,archaea  
+ncbi-genome-download --formats fasta bacteria,viral,fungi,protozoa,archaea  
 
-cd RefSeq
+cd refseq
 
 zcat \*/GCF\*/\*fna.gz |sed 's/^>/>contam-/g'  > contaminant.fasta
+cd ../
 
 #also download human reference genome
 wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/000/001/405/GCA_000001405.28_GRCh38.p13/GCA_000001405.28_GRCh38.p13_genomic.fna.gz
 zcat GCA_000001405.28_GRCh38.p13_genomic.fna.gz |sed 's/^>/>human-/g' > human.fasta
 
-#repeat the same with insect genome and insert id
+#repeat the same with insect genome and insert id:
+cd insect/
+zcat */*fna.gz  |sed 's/^>/>insect/g' > insect.fasta 
+#/!\ warning: change according to your model organisms outgroups genomes
 
 #concatenate all:
 cat contaminant.fasta insect.fasta human.fasta > insect_human_contam.fa
