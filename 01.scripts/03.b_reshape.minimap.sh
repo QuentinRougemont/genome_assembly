418ea2343188d8eae39ef7e73b49a6c9bd011a8c
Quentin Rougemont <quentin@GEEServ01.ese.u-psud.fr>
2023-06-14 13:01:20 +0200

    fixed typo

--- 01.scripts/03.b_reshape.minimap.sh
+++ 01.scripts/03.b_reshape.minimap.sh
@@ -1,11 +1,11 @@
 
 
 #separate minimap alignment based on matching pattern:
-grep -v "SQ" minimap2.sam.gz |awk '$3=="insect" {print $1"\t"$2"\t"$3"\t"$4"\t"$5}' |gzip > insect.txt.gz
+zgrep -v "SQ" minimap2.sam.gz |awk '$3=="insect" {print $1"\t"$2"\t"$3"\t"$4"\t"$5}' |gzip > insect.txt.gz
 
-grep -v "SQ" minimap2.sam.gz |awk '$3=="contam" {print $1"\t"$2"\t"$3"\t"$4"\t"$5}' |gzip > contam.txt.gz
+zgrep -v "SQ" minimap2.sam.gz |awk '$3=="contam" {print $1"\t"$2"\t"$3"\t"$4"\t"$5}' |gzip > contam.txt.gz
 
-grep -v "SQ" minimap2.sam.gz |awk '$3=="human" {print $1"\t"$2"\t"$3"\t"$4"\t"$5}' |gzip > human.txt.gz
+zgrep -v "SQ" minimap2.sam.gz |awk '$3=="human" {print $1"\t"$2"\t"$3"\t"$4"\t"$5}' |gzip > human.txt.gz
 
 
 #Also take time to look at the mapping code ($6) and look at the blast results
