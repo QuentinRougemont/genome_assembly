9868ff8066d2d266098996c5e695c08e20442d8c
Quentin Rougemont <quentinrougemont@orange.fr>
2024-01-15 21:39:53 +0100

    Update 02.download_contaminant_human_and_insect.sh

--- 01.scripts/02.download_contaminant_human_and_insect.sh
+++ 01.scripts/02.download_contaminant_human_and_insect.sh
@@ -1,3 +1,11 @@
+#puprose: 
+#script to download many genome with ncbi-genome-download tools on the ncbi database usign refseq categories
+#Author: QR
+#date 2022
+#update: 2024
+
+species=$1 #name of a focal species (major lineage) of interest to be download on ncbi
+
 
 #use ncbi dowload to dowload the data: https://github.com/kblin/ncbi-genome-download
 #ncbi-genome-download --formats fasta --refseq-categories reference bacteria,viral,fungi,protozoa,archaea  
@@ -12,12 +20,12 @@ cd ../
 wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/000/001/405/GCA_000001405.28_GRCh38.p13/GCA_000001405.28_GRCh38.p13_genomic.fna.gz
 zcat GCA_000001405.28_GRCh38.p13_genomic.fna.gz |sed 's/^>/>human-/g' > human.fasta
 
-#repeat the same with insect genome and insert id:
-cd insect/
-zcat */*fna.gz  |sed 's/^>/>insect/g' > insect.fasta 
-#/!\ warning: change according to your model organisms outgroups genomes
+#repeat the same with $species genome and insert id:
+ncbi-genome-download --formats fasta $speices
+
+zcat $species/*/*fna.gz  |sed "s/^>/>$species/g" > "species".fasta 
 
 #concatenate all:
-cat contaminant.fasta insect.fasta human.fasta > insect_human_contam.fa
+cat contaminant.fasta $species.fasta human.fasta  |gzip > "$species"_human_contam.fa.gz
 
 #data should be ready for alignment with minimap
