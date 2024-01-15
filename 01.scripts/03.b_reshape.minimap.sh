b7d10b51c4b0e43408661ffafc8029f4d3693ab1
Quentin Rougemont <quentinrougemont@orange.fr>
2024-01-15 21:44:03 +0100

    Update 03.b_reshape.minimap.sh

--- 01.scripts/03.b_reshape.minimap.sh
+++ 01.scripts/03.b_reshape.minimap.sh
@@ -1,11 +1,13 @@
 
 
 #separate minimap alignment based on matching pattern:
-zgrep -v "SQ" minimap2.sam.gz |awk '$3 ~/insect/ {print $1"\t"$2"\t"$3"\t"$4"\t"$5}' |gzip > insect.txt.gz
+species=$1
 
-zgrep -v "SQ" minimap2.sam.gz |awk '$3 ~/contam/ {print $1"\t"$2"\t"$3"\t"$4"\t"$5}' |gzip > contam.txt.gz
+zgrep -v "SQ" minimap2.sam.gz |awk -v spe=species '$3 ~ spe && $2!=4 {print $1"\t"$2"\t"$3"\t"$4"\t"$5}' |gzip > "$species".txt.gz
 
-zgrep -v "SQ" minimap2.sam.gz |awk '$3 ~/human/ {print $1"\t"$2"\t"$3"\t"$4"\t"$5}' |gzip > human.txt.gz
+zgrep -v "SQ" minimap2.sam.gz |awk '$3 ~/contam/ && $2!=4 {print $1"\t"$2"\t"$3"\t"$4"\t"$5}' |gzip > contam.txt.gz
+
+zgrep -v "SQ" minimap2.sam.gz |awk '$3 ~/human/ && $2!=4 {print $1"\t"$2"\t"$3"\t"$4"\t"$5}' |gzip > human.txt.gz
 
 
 #Also take time to look at the mapping code ($6) and look at the blast results
