cee9e18a41bda4e77bc57e741d0d06b527f70590
Quentin Rougemont <quentinrougemont@orange.fr>
2024-02-22 14:59:46 +0100

    Update 00.hifi.length.sh

--- 01.scripts/00.hifi.length.sh
+++ 01.scripts/00.hifi.length.sh
@@ -1,28 +1,10 @@
 
-#compute length of the raw hifi file 
-#file generate with extract hifi to get QV 20 (0.99) reads
+#compute length of the raw hifi file and GC content
+#file generated with extract hifi to get QV 20 (0.99) reads
 
 input=$1
-zcat $input | awk '{if(NR%4==1) {printf(">%s\t",substr($0,2));} else if(NR%4==2) print length;}' |gzip > $input.len.gz
-
+#zcat $input | awk '{if(NR%4==1) {printf(">%s\t",substr($0,2));} else if(NR%4==2) print length;}' |gzip > $input.len.gz
+zcat $input | awk '{if(NR%4==1) {printf(">%s\t",substr($0,2));} else if(NR%4==2) print length "\t" gsub(/[GC]/, "")}' |awk '{print $0"\t"$3/$2}'  |gzip > $input.len.gc.gz 
 
 #then plot histogram in R
-
-argv <- commandArgs(T)
- 
-a <- read.table(argv[1])
-
-mean = data.frame(mean(a$V2))
-colnames(mean) = "mean"
-
-
-library(ggplot2)
-
-pdf(file = "hist.len.pdf", 8,8)
-ggplot(a, aes(x = V2, color="z") ) + geom_histogram(fill = "white")  + 
-   theme_classic() + 
-   geom_vline(data = mean, aes(xintercept = mean) , linetype = "dashed") + 
-   xlab("read len") + 
-   ylab("count") + 
-   scale_color_brewer(palette="Dark2") + theme(legend.position="none")
-dev.off()
+Rscript 01.scripts/Rscripts/plot_len_gc.R $input.len.gc.gz
